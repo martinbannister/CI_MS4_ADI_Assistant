fc1dfe714a538c47952d6a201d9864fc4522b6ed
martinbannister <martin.bannister91@gmail.com>
2022-04-01 17:31:06 +0100

    FEAT(transactions): customise admin for transactions & accounting codes

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,5 +2,42 @@ from django.contrib import admin
 from .models import Transaction, AccountingCode
 
 
-admin.site.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ('transaction_uuid', 'amount')
+
+    fields = (
+        'transaction_uuid',
+        'owner',
+        'transaction_date',
+        'accounting_code',
+        'customer',
+        'description',
+        'rate',
+        'hours',
+        'amount_in',
+        'amount_out',
+        'amount',
+    )
+
+    list_display = (
+        'transaction_uuid',
+        'owner',
+        'description',
+        'amount',
+    )
+
+    ordering = ('transaction_date',)
+
+
+class AccountingCodeAdmin(admin.ModelAdmin):
+    list_display = (
+        'owner',
+        'code',
+        'description',
+    )
+
+    ordering = ('owner', 'code',)
+
+
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(AccountingCode)
