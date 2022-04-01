6655cad85594db47efec255b4dead8e4b433e966
martinbannister <martin.bannister91@gmail.com>
2022-04-01 20:57:10 +0100

    FEAT(customers): customise admin for progress & subjects

--- customers/admin.py
+++ customers/admin.py
@@ -7,8 +7,29 @@ from .models import (
                      ProgressItem
                     )
 
+
+class ProgressItemsAdminInline(admin.TabularInline):
+    model = ProgressItem
+    list_display = (
+        'subject',
+        'score',
+        'comments',
+        'practice_next_lesson',
+    )
+
+
+class ProgressAdmin(admin.ModelAdmin):
+    inlines = (ProgressItemsAdminInline,)
+
+
+class SubjectAdminInline(admin.TabularInline):
+    model = Subject
+
+
+class SubjectCategoryAdmin(admin.ModelAdmin):
+    inlines = (SubjectAdminInline,)
+
+
 admin.site.register(Customer)
-admin.site.register(SubjectCategory)
-admin.site.register(Subject)
-admin.site.register(Progress)
-admin.site.register(ProgressItem)
+admin.site.register(SubjectCategory, SubjectCategoryAdmin)
+admin.site.register(Progress, ProgressAdmin)
