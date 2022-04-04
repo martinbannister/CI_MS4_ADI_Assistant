a587274318d9612d83c3802b00cd0546f0c13adc
martinbannister <martin.bannister91@gmail.com>
2022-04-04 08:26:27 +0100

    FEAT(transactions): add acc code detail, edit & delete paths

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,7 +1,31 @@
 from django.urls import path
 
-from .views import AccountingCodeListView
+from .views import (
+    AccountingCodeListView,
+    AccoutingCodeDetailView,
+    AccoutingCodeEditView,
+    AccoutingCodeDeleteView
+    )
 
 urlpatterns = [
-    path("accountscodes/", AccountingCodeListView.as_view(), name='acc_code_list')
+    path(
+        '<int:pk>/',
+        AccoutingCodeDetailView.as_view(),
+        name='acc_code_detail'
+        ),
+    path(
+        '<int:pk>/edit/',
+        AccoutingCodeEditView.as_view(),
+        name='acc_code_edit'
+    ),
+    path(
+        '<int:pk>/delete',
+        AccoutingCodeDeleteView.as_view(),
+        name='acc_code_delete'
+    ),
+    path(
+        "accountscodes/",
+        AccountingCodeListView.as_view(),
+        name='acc_code_list'
+        ),
 ]
