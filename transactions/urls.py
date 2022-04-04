bfe16b10c390be9a3286c3a36398b032aeb0b8ee
martinbannister <martin.bannister91@gmail.com>
2022-04-04 23:14:43 +0100

    FEAT(transactions): add transactions views, urls & templates

--- transactions/urls.py
+++ transactions/urls.py
@@ -6,6 +6,11 @@ from .views import (
     AccoutingCodeEditView,
     AccoutingCodeDeleteView,
     AccountingCodeCreateView,
+    TransactionListView,
+    TransactionDetailView,
+    TransactionEditView,
+    TransactionDeleteView,
+    TransactionCreateView,
     )
 
 urlpatterns = [
@@ -33,5 +38,31 @@ urlpatterns = [
         "codes/",
         AccountingCodeListView.as_view(),
         name='acc_code_list'
+        ),
+    # Transactions
+    path(
+        '<int:pk>/',
+        TransactionDetailView.as_view(),
+        name='trans_detail'
+        ),
+    path(
+        '<int:pk>/edit/',
+        TransactionEditView.as_view(),
+        name='trans_edit'
+    ),
+    path(
+        '<int:pk>/delete/',
+        TransactionDeleteView.as_view(),
+        name='trans_delete'
+    ),
+    path(
+        'new/',
+        TransactionCreateView.as_view(),
+        name='trans_create'
+    ),
+    path(
+        '',
+        TransactionListView.as_view(),
+        name='trans_list'
         ),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Transaction, AccountingCode
+from .forms import TransactionForm
 
 
 class AccountingCodeListView(ListView):
@@ -36,6 +37,41 @@ class AccountingCodeCreateView(CreateView):
         'code',
         'description',
     )
+
+    # REF: https://docs.djangoproject.com/en/dev/topics/class-based-views/generic-editing/#models-and-request-user
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+# Transactions
+
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'trans_list.html'
+
+
+class TransactionDetailView(DetailView):
+    model = Transaction
+    template_name = 'trans_detail.html'
+
+
+class TransactionEditView(UpdateView):
+    model = Transaction
+    template_name = 'trans_edit.html'
+    form_class = TransactionForm
+
+
+class TransactionDeleteView(DeleteView):
+    model = Transaction
+    template_name = 'trans_delete.html'
+    success_url = reverse_lazy('trans_list.html')
+
+
+class TransactionCreateView(CreateView):
+    model = Transaction
+    template_name = 'trans_create.html'
+    form_class = TransactionForm
 
     # REF: https://docs.djangoproject.com/en/dev/topics/class-based-views/generic-editing/#models-and-request-user
     def form_valid(self, form):
