17989bba7534282ae245837deac6dc7db4733d1d
martinbannister <martin.bannister91@gmail.com>
2022-04-04 23:34:58 +0100

    DOCS(transactions): add reference for option['name'] conversion string

--- transactions/forms.py
+++ transactions/forms.py
@@ -7,6 +7,7 @@ class BlankSelect(forms.Select):
     def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
         option = super().create_option(name, value, label, selected, index, subindex, attrs)
         if not value:
+            # REF: https://stackoverflow.com/questions/8347048/how-to-convert-string-to-title-case-in-python#comment-28452851
             option['label'] = f"Select {option['name'].replace('_', ' ').title()}"
         return option
 
