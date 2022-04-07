61a20a9391e3c3370ec27c3fb5d73d20c3a74c22
martinbannister <martin.bannister91@gmail.com>
2022-04-07 19:32:16 +0100

    SECURITY: implement decouple for secret values

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3=)-z(q3%3t3chy*-gpq8xo&b-&dd%mse99ixr7_k0rmozkt_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'customers.apps.CustomersConfig',
     'pages',
     'transactions',
+    'payments.apps.PaymentsConfig'
 ]
 
 MIDDLEWARE = [
