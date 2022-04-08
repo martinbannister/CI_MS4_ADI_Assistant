1dcc10fc9881e7c92c7b810c079183aa8542c700
martinbannister <martin.bannister91@gmail.com>
2022-04-08 13:24:33 +0100

    FEAT(payments): create view, url & template and add css for checkout

--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from .views import checkout
 
 urlpatterns = [
     path('stripe/', include('djstripe.urls', namespace='djstripe')),
+    path('checkout/', checkout, name='checkout'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import stripe
+import json
+from django.http import JsonResponse
+from djstripe.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def checkout(request):
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
+    return render(request, "payments_checkout.html", context)
