1217b1d6478720470b7672c7c58686aa8b6c7d6c
martinbannister <martin.bannister91@gmail.com>
2022-04-11 14:17:14 +0100

    FEAT(accounts): add inline profile to custom user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,8 +4,18 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+from profiles.models import UserProfile
+
+
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profile'
+    fk_name = 'user'
+
 
 class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
@@ -23,6 +33,11 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
     add_fieldsets = UserAdmin.fieldsets
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
