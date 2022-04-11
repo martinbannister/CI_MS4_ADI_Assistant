cc17f80468d87ff535dafa051b4bbbb3911a6240
martinbannister <martin.bannister91@gmail.com>
2022-04-11 21:45:16 +0100

    FIX(settings): correct allowed hosts for heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'ci-ms4-adi-assistant',
+    'ci-ms4-adi-assistant.herokuapp.com',
     '127.0.0.1'
 ]
 
